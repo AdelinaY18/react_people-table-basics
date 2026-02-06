06b45d85316dcf54f6e114a7150622ae71ccb1d5
Adelina Yovdii <adelincaiovdii@gmail.com>
2026-02-06 21:21:08 +0200

    solution

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -22,6 +22,7 @@ export const PeoplePage = () => {
 
   useEffect(() => {
     setLoading(true);
+    setError(false);
 
     fetch('https://mate-academy.github.io/react_people-table/api/people.json')
       .then(res => res.json())
