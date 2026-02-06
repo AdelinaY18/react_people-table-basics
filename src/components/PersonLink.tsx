ed207e0c96a42f5fd0b3dc6eac8bef755689af92
Adelina Yovdii <adelincaiovdii@gmail.com>
2026-02-06 21:10:09 +0200

    add task solution

---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+export const PersonLink = ({ person, name, people }) => {
+  if (person) {
+    return (
+      <Link
+        to={`/people/${person.slug}`}
+        className={person.sex === 'f' ? 'has-text-danger' : ''}
+      >
+        {person.name}
+      </Link>
+    );
+  }
+
+  const found = people?.find(p => p.name === name);
+
+  if (!found) {
+    return name;
+  }
+
+  return (
+    <Link
+      to={`/people/${found.slug}`}
+      className={found.sex === 'f' ? 'has-text-danger' : ''}
+    >
+      {name}
+    </Link>
+  );
+};
---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Loader } from '../components/Loader';
+import { PeopleTable } from '../components/PeopleTable';
+
+export interface Person {
+  slug: string;
+  name: string;
+  sex: string;
+  born: number;
+  died: number | null;
+  fatherName: string | null;
+  motherName: string | null;
+}
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const { slug } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('https://mate-academy.github.io/react_people-table/api/people.json')
+      .then(res => res.json())
+      .then(data => {
+        setPeople(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="block">
+      <div className="box table-container">
+        <h1 className="title">People Page</h1>
+        {loading && <Loader />}
+
+        {error && (
+          <p data-cy="peopleLoadingError" className="has-text-danger">
+            Something went wrong
+          </p>
+        )}
+
+        {!loading && !error && people.length === 0 && (
+          <p data-cy="noPeopleMessage">There are no people on the server</p>
+        )}
+
+        {!loading && !error && people.length > 0 && (
+          <PeopleTable people={people} selectedSlug={slug} />
+        )}
+      </div>
+    </div>
+  );
+};
